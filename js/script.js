d9ffe358b05d9d8fbbef02a0618b4201b64f086d
Rod <rod.bromberg@yahoo.com>
2020-01-17 18:14:16 -0500

    update

--- js/script.js
+++ js/script.js
@@ -95,29 +95,29 @@ $(document).ready(function() {
 
     return false;
   });
-
-  $("#navigation li a").click(function(event) {
-    event.preventDefault();
-
-    var targetElement = $(this).attr("href");
-    var targetPosition = $(targetElement).offset().top;
-    $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
-  });
-
-  const nav = $("#navigation");
-  const navTop = nav.offset().top;
-
-  $(window).on("scroll", stickyNavigation);
-
-  function stickyNavigation() {
-    var body = $("body");
-
-    if ($(window).scrollTop() >= navTop) {
-      body.css("padding-top", nav.outerHeight() + "px");
-      body.addClass("fixedNav");
-    } else {
-      body.css("padding-top", 0);
-      body.removeClass("fixedNav");
-    }
-  }
 });
+
+// $("#navigation li a").click(function(event) {
+//   event.preventDefault();
+
+//   var targetElement = $(this).attr("href");
+//   var targetPosition = $(targetElement).offset().top;
+//   $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+// });
+
+// const nav = $("#navigation");
+// const navTop = nav.offset().top;
+
+// $(window).on("scroll", stickyNavigation);
+
+// function stickyNavigation() {
+//   var body = $("body");
+
+//   if ($(window).scrollTop() >= navTop) {
+//     body.css("padding-top", nav.outerHeight() + "px");
+//     body.addClass("fixedNav");
+//   } else {
+//     body.css("padding-top", 0);
+//     body.removeClass("fixedNav");
+//   }
+// }
